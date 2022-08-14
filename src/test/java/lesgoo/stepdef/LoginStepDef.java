78f27d12e054cc1c3f6a507f098b2d026c6e585d
dhandyjoe <dhanjoenkp@gmail.com>
2022-08-14 13:28:12 +0700

    add login and logout

---
+++ src/test/java/lesgoo/stepdef/LoginStepDef.java
@@ -0,0 +1,50 @@
+package lesgoo.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import lesgoo.api.LesGooApi;
+import lesgoo.response.LesGooResponse;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
+import org.json.JSONObject;
+
+import static org.hamcrest.Matchers.equalTo;
+
+
+import java.io.File;
+
+
+public class LoginStepDef {
+
+    @Steps
+    LesGooApi lesGooApi;
+
+    @When("login with username {string} and password {string}")
+    public void getListUserWithParameter(String username, String password) {
+//        File jsonFile = new File(LesGooApi.DIR+"/src/test/resources/json/requestbody/login/LoginWithValidData.json");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", username);
+        jsonObject.put("password", password);
+        jsonObject.put("fcm_token", "fcmtoken");
+        String login = jsonObject.toString();
+
+        lesGooApi.loginUser(login);
+    }
+
+    @When("Send request login")
+    public void sendRequestLogin() {
+        SerenityRest.when().post(LesGooApi.LOGIN);
+    }
+
+    @Then("Status code should be {int}")
+    public void statusCodeShouldBe(int statusCode) {
+        SerenityRest.then().statusCode(statusCode);
+    }
+
+    @And("Response body should contain message {string}")
+    public void responseBodyShouldContainMessage(String message) {
+        SerenityRest.then().body(LesGooResponse.MESSAGE, equalTo(message));
+    }
+}
